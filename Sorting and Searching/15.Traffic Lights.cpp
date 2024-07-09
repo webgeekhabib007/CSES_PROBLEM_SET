9a836dc031956d6b3014bb521231f893123eae54
webgeekhabib007 <hr665102@gmail.com>
2024-07-09 17:48:50 +0600

    usual git commit again

--- Sorting and Searching/15.Traffic Lights.cpp
+++ Sorting and Searching/15.Traffic Lights.cpp
@@ -22,12 +22,9 @@ int main(int argc,char** argv){
         auto it = s.lower_bound(a);
         auto it2 = it;
         --it2;
- 		//debug2(*it,*it2)
         ms.erase(ms.find(*it - *it2));
         ms.insert(a - *it2);
         ms.insert(*it - a);
- 		//joker(ms)
-        //auto last = ms.end()[1];
         cout << *--ms.end() << " ";
         s.insert(a);
     }
